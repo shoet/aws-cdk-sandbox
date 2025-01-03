170a5a5f73278bab00751dd4dbb8b4d178af3fc4
shoet <32211937+shoet@users.noreply.github.com>
2025-01-03 10:17:40 +0900

    CloudFrontを前段に配置しLambdaFunctionURLsの実行を許可する

--- ts-nextjs-lambda-example-oac/infrastracture/lib/infrastracture-stack.ts
+++ ts-nextjs-lambda-example-oac/infrastracture/lib/infrastracture-stack.ts
@@ -1,11 +1,13 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { LambdaFunctionStack } from "../lib/lambda-function-stack";
+import { CloudFront } from "./CloudFront";
+import { LambdaFunctionStack } from "./lambda-function-stack";
 
 export class MainStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new LambdaFunctionStack(this, "LambdaFunctionStack");
+    const lambda = new LambdaFunctionStack(this, "LambdaFunctionStack");
+    new CloudFront(this, "CloudFront", lambda.function, lambda.functionUrl);
   }
 }
